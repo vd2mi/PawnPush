32d1134c650d9a5165947903df54e279782558f5
abdulrahman alahmed <Abdul.Alahmed2004@gmail.com>
2025-10-07 01:28:56 +0300

    trying a new solve

--- survival.js
+++ survival.js
@@ -1,5 +1,22 @@
-import { db } from "./api/firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
 import { collection, addDoc, getDocs, doc, updateDoc, query, orderBy, limit } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBz6YA8veeDSY-naBqYdafJbcnfsnDi3EY",
+  authDomain: "pawnpush-leaderboard.firebaseapp.com",
+  projectId: "pawnpush-leaderboard",
+  storageBucket: "pawnpush-leaderboard.appspot.com",
+  messagingSenderId: "948510714839",
+  appId: "1:948510714839:web:4e31534f1b4f305fa57992",
+  measurementId: "G-F0QKLGBD7P"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 function playSound(soundName) {
   if (window.audioManager) {
